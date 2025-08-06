a6b9626f130a46960baf77e023ebde2191d50d3a
jnthntena <jonathanarroyo347@gmail.com>
2025-08-06 12:02:25 -0500

    Init database

---
+++ EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeeAdminPortal.Models.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+
+        public required string Email { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
